c603eb325e8f76d061f1fa46644ca5657e4b6421
Jonas Schievink <jonasschievink@gmail.com>
2018-06-15 04:21:35 +0200

    Actually print the time zone
    who needs those anyways right?
    
    Also complete the datetime formatting implementation.

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use iso8601;
+use iso8601::{Date, Time, DateTime};
 use xml::escape::escape_str_pcdata;
 
 use std::borrow::Cow;
@@ -10,17 +10,25 @@ pub fn escape_xml(s: &str) -> Cow<str> {
     escape_str_pcdata(s)
 }
 
-pub fn format_datetime(date_time: &iso8601::DateTime) -> String {
-    let iso8601::Time { hour, minute, second, .. } = date_time.time;
+pub fn format_datetime(date_time: &DateTime) -> String {
+    let Time {
+        hour, minute, second, millisecond, tz_offset_hours, tz_offset_minutes
+    } = date_time.time;
     
     match date_time.date {
-        iso8601::Date::YMD { year, month, day } => {
-            format!("{:04}{:02}{:02}T{:02}:{:02}:{:02}",
+        Date::YMD { year, month, day } => {
+            format!("{:04}{:02}{:02}T{:02}:{:02}:{:02}.{:.3}{:+03}:{:02}",
                 year, month, day,
-                hour, minute, second
+                hour, minute, second, millisecond,
+                tz_offset_hours, tz_offset_minutes.abs()
             )
         }
-        _ => { unimplemented!() }
+        Date::Week { year, ww, d } => {
+            format!("{:04}-W{:02}-{}", year, ww, d)
+        }
+        Date::Ordinal { year, ddd } => {
+            format!("{:04}-{:03}", year, ddd)
+        }
     }
 }
 
@@ -31,8 +39,10 @@ mod tests {
 
     #[test]
     fn formats_datetimes() {
-        let date_time = iso8601::datetime("2016-05-02T06:01:05-0800").unwrap();
+        let date_time = iso8601::datetime("2016-05-02T06:01:05-0830").unwrap();
 
-        assert_eq!(format_datetime(&date_time), "20160502T06:01:05");
+        let formatted = format_datetime(&date_time);
+        assert_eq!(formatted, "20160502T06:01:05.0-08:30");
+        assert_eq!(iso8601::datetime(&formatted).unwrap(), date_time);
     }
 }
