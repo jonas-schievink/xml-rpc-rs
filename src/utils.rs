d3f6279842aee05879eca009c8bcca3ea26bd5fe
Jonas Schievink <jonasschievink@gmail.com>
2018-06-15 13:32:23 +0200

    Format datetimes for interoperability
    This removes the recently added support for year+week and ordinal
    representations because they are likely not supported by many clients.
    Also, this now only formats fractions and timezone data if they're
    non-zero because they also might not supported by clients.

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@ use iso8601::{Date, Time, DateTime};
 use xml::escape::escape_str_pcdata;
 
 use std::borrow::Cow;
+use std::fmt::Write;
 
 /// Escape a string for use as XML characters.
 ///
@@ -10,6 +11,13 @@ pub fn escape_xml(s: &str) -> Cow<str> {
     escape_str_pcdata(s)
 }
 
+/// Formats a `DateTime` for use in XML-RPC.
+///
+/// Note that XML-RPC is extremely underspecified when it comes to datetime values. Apparently,
+/// some clients [don't even support timezone information][wp-bug] (we do). For maximum
+/// interoperability, this will omit fractional time and time zone if not specified.
+///
+/// [wp-bug]: https://core.trac.wordpress.org/ticket/1633#comment:4
 pub fn format_datetime(date_time: &DateTime) -> String {
     let Time {
         hour, minute, second, millisecond, tz_offset_hours, tz_offset_minutes
@@ -17,17 +25,26 @@ pub fn format_datetime(date_time: &DateTime) -> String {
     
     match date_time.date {
         Date::YMD { year, month, day } => {
-            format!("{:04}{:02}{:02}T{:02}:{:02}:{:02}.{:.3}{:+03}:{:02}",
+            // The base format is based directly on the example in the spec and should always work:
+            let mut string = format!("{:04}{:02}{:02}T{:02}:{:02}:{:02}",
                 year, month, day,
-                hour, minute, second, millisecond,
-                tz_offset_hours, tz_offset_minutes.abs()
-            )
+                hour, minute, second
+            );
+            // Only append milliseconds when they're >0
+            if millisecond > 0 {
+                write!(string, ".{:.3}", millisecond).unwrap();
+            }
+            // Only append time zone info if the offset is specified and not 00:00
+            if tz_offset_hours != 0 || tz_offset_minutes != 0 {
+                write!(string, "{:+03}:{:02}", tz_offset_hours, tz_offset_minutes.abs()).unwrap();
+            }
+
+            string
         }
-        Date::Week { year, ww, d } => {
-            format!("{:04}-W{:02}-{}", year, ww, d)
-        }
-        Date::Ordinal { year, ddd } => {
-            format!("{:04}-{:03}", year, ddd)
+        // Other format are just not supported at all:
+        Date::Week { .. }
+        | Date::Ordinal { .. } => {
+            unimplemented!()
         }
     }
 }
@@ -42,7 +59,19 @@ mod tests {
         let date_time = iso8601::datetime("2016-05-02T06:01:05-0830").unwrap();
 
         let formatted = format_datetime(&date_time);
-        assert_eq!(formatted, "20160502T06:01:05.0-08:30");
+        assert_eq!(formatted, "20160502T06:01:05-08:30");
+        assert_eq!(iso8601::datetime(&formatted).unwrap(), date_time);
+
+        // milliseconds / fraction
+        let date_time = iso8601::datetime("20160502T06:01:05.400").unwrap();
+        let formatted = format_datetime(&date_time);
+        assert_eq!(formatted, "20160502T06:01:05.400");
+        assert_eq!(iso8601::datetime(&formatted).unwrap(), date_time);
+
+        // milliseconds / fraction + time zone
+        let date_time = iso8601::datetime("20160502T06:01:05.400+01:02").unwrap();
+        let formatted = format_datetime(&date_time);
+        assert_eq!(formatted, "20160502T06:01:05.400+01:02");
         assert_eq!(iso8601::datetime(&formatted).unwrap(), date_time);
     }
 }
